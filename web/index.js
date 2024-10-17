fd2ad533f878af455e333fd65dce15f47a0b8256
aiplayuser <167263663+aiplayuser@users.noreply.github.com>
2024-10-17 16:07:27 +0800

    Add files via upload

--- web/index.js
+++ web/index.js
@@ -7,8 +7,6 @@ const link = document.createElement("link"); link.rel = "stylesheet";
 link.href = "extensions/comfyui_controlnet_aux_pre/index.css";
 document.head.appendChild(link);
 
-let preprolist = {}; let controlnet = 'control_v11p_sd15_canny.pth';
-
 function listsort(listdata,valuestr) { listdata.sort((a,b)=> valuestr.includes(b.name) - valuestr.includes(a.name)); };
 
 function addhide(el,searchValue) { el.classList.toggle('hide', !( 
@@ -19,31 +17,36 @@ function addhide(el,searchValue) { el.classList.toggle('hide', !(
 function getTagList() {  return preprolist[controlnet].map((tag, index) => { 
     return $el('label.Preprocessor-tag',   
                 { dataset: { tag: tag.name, name: tag.name, index: index },  
-                  $: (el) => { el.firstChild.onclick = () => { el.classList.toggle("Preprocessor-tag-selected"); }; },  },   
-                [ $el("input", { type: 'checkbox', name: tag.name }), $el("span", { textContent: tag.name })  ] ); } );  };
+                  $: (el) => { el.firstChild.onclick = () => { 
+                    document.querySelectorAll(`.Preprocessor-tag`).forEach(tr => tr.classList.remove("Preprocessor-tag-selected"));
+                    el.classList.toggle("Preprocessor-tag-selected"); }; },  },   
+                [ $el("input", { type: 'checkbox', name: tag.name, value: tag.name }), 
+                  $el("span", { textContent: tag.name })  ] 
+            ); })};
+
+let preprolist = {}; let controlnet = 'control_v11p_sd15_canny.pth';
 
 async function getprepro(el){ const resp = await api.fetchApi(`/Preprocessor?name=${controlnet}`);
-    if (resp.status === 200) { let data = await resp.json(); let mlist = ["","none"];
+    preprolist[controlnet] = await resp.json(); let mlist = ["","none"];
+    if(controlnet.includes("canny")){ mlist=["canny", "CannyEdgePreprocessor"] }
+    else if(controlnet.includes("depth")){ mlist=["depth", "DepthAnythingV2Preprocessor"] }
+    else if(controlnet.includes("lineart")){ mlist=["lineart", "LineartStandardPreprocessor"] }
+    else if(controlnet.includes("tile")){ mlist=["tile", "TilePreprocessor"] }
+    else if(controlnet.includes("scrib")){ mlist=["scrib", "Scribble_XDoG_Preprocessor"] }
+    else if(controlnet.includes("soft")){ mlist=["soft", "HEDPreprocessor"] }
+    else if(controlnet.includes("pose")){ mlist=["pose", "DWPreprocessor"] }    
+    else if(controlnet.includes("normal")){ mlist=["normal", "BAE-NormalMapPreprocessor"] }
+    else if(controlnet.includes("seg")){ mlist=["semseg", "OneFormer-COCO-SemSegPreprocessor"] }
+    else if(controlnet.includes("shuffle")){ mlist=["shuffle", "ShufflePreprocessor"] }
+    else if(controlnet.includes("ioclab_sd15_recolor")){ mlist=["image", "ImageLuminanceDetector"] }
+    document.querySelector('.searchpre').value = mlist[0]; listsort(preprolist[controlnet],mlist[1]);
+    el.innerHTML = ''; el.append(...getTagList()); updatalist(el); };
 
-        if(controlnet.includes("canny")){ mlist=["canny", "CannyEdgePreprocessor"] }
-        else if(controlnet.includes("depth")){ mlist=["depth", "MiDaS-DepthMapPreprocessor"] }
-        else if(controlnet.includes("lineart")){ mlist=["lineart", "LineArtPreprocessor"] }
-        else if(controlnet.includes("tile")){ mlist=["tile", "TilePreprocessor"] }
-        else if(controlnet.includes("scrib")){ mlist=["scrib", "FakeScribblePreprocessor"] }
-        else if(controlnet.includes("soft")){ mlist=["soft", "HEDPreprocessor"] }
-        else if(controlnet.includes("pose")){ mlist=["pose", "DWPreprocessor"] }    
-        else if(controlnet.includes("normal")){ mlist=["normal", "BAE-NormalMapPreprocessor"] }
-        else if(controlnet.includes("semseg")){ mlist=["semseg", "OneFormer-ADE20K-SemSegPreprocessor"] }
-        else if(controlnet.includes("shuffle")){ mlist=["shuffle", "ShufflePreprocessor"] }
-        else if(controlnet.includes("ioclab_sd15_recolor")){ mlist=["image", "ImageLuminanceDetector"] }
-        else if(controlnet.includes("t2iadapter_color")){ mlist=["color", "ColorPreprocessor"] }
-        else if(controlnet.includes("sketch")){ mlist=["scrib", "FakeScribblePreprocessor"] }
-        
-        document.querySelector('.search').value = mlist[0]; listsort(data,mlist[1]);
-
-        preprolist[controlnet] = data; el.innerHTML = ''; el.append(...getTagList()); 
-        el.children[0].classList.add("Preprocessor-tag-selected"); el.children[0].children[0].checked = true;
-    } };
+function updatalist(els) { const searchValue = document.querySelector('.searchpre').value;  //获取搜索框文本
+    const selectedTags = Array.from(els.querySelectorAll('.Preprocessor-tag-selected')).map(el => el.dataset.tag); // 当前选中的标签
+    listsort(preprolist[controlnet],selectedTags); els.innerHTML = ''; els.append(...getTagList());  // 重新排序
+    els.children[0].classList.add("Preprocessor-tag-selected"); els.children[0].children[0].checked = true;
+    els.querySelectorAll('.Preprocessor-tag').forEach(el => { addhide(el,searchValue); }); };  // 同时处理搜索和隐藏逻辑 
 
 app.registerExtension({
     name: 'comfy.ControlNet Preprocessors.Preprocessor Selector',
@@ -53,50 +56,41 @@ app.registerExtension({
                 this.setProperty("values",[]); this.setSize([300, 350]); 
                 
                 const toolsElement = $el('div.tools', [         //添加清空按钮搜索框 
-                    $el('button.delete',{ textContent: 'Empty',
-                        onclick:()=>{ selectorlist[0].querySelectorAll(".search").forEach(el=>{ el.value = '' });
+                    $el('button.Empty',{ textContent: 'Empty',
+                        onclick:()=>{ selectorlist[0].querySelectorAll(".searchpre").forEach(el=>{ el.value = '' });
                                       selectorlist[1].querySelectorAll(".Preprocessor-tag").forEach(el => {
                                           el.classList.remove("Preprocessor-tag-selected"); 
                                           el.classList.remove("hide"); el.children[0].checked = false }) } }),
                             
-                    $el('textarea.search',{ placeholder:"🔎 search",
-                        oninput:(e)=>{ let searchValue = e.target.value;
-                            selectorlist[1].querySelectorAll(".Preprocessor-tag").forEach(el => {  addhide(el,searchValue); }) } }) 
+                    $el('textarea.searchpre',{ placeholder:"🔎 searchpre",
+                        oninput:(e)=>{ selectorlist[1].querySelectorAll(".Preprocessor-tag").forEach(el => { addhide(el,e.target.value); }) } }) 
                 ]);                
                 const stylesList = $el("ul.Preprocessor-list", []);               
                 let selector = this.addDOMWidget( 'select_styles', "btn", $el('div.Preprocessor', [toolsElement, stylesList] ) ); 
                 let selectorlist = selector.element.children;
 
                 // 监听鼠标离开事件  
-                selectorlist[1].addEventListener('mouseleave', function(e) { const searchValue = document.querySelector('.search').value;  
-                    const selectedTags = Array.from(this.querySelectorAll('.Preprocessor-tag-selected')).map(el => el.dataset.tag); // 当前选中的标签值
-                    listsort(preprolist[controlnet],selectedTags); this.innerHTML = ''; this.append(...getTagList());  // 重新排序
-                    this.querySelectorAll('.Preprocessor-tag').forEach(el => {  // 遍历所有标签
-                        const isSelected = selectedTags.includes(el.dataset.tag); //标签的选中状态
-                        if (isSelected) { el.classList.add("Preprocessor-tag-selected"); el.children[0].checked = true; } //更新样式标签的选中状态 
-                        addhide(el,searchValue); });   // 同时处理搜索和隐藏逻辑 
-                });            
+                selectorlist[1].addEventListener('mouseleave', function(e) { updatalist(this); });            
                  
                 //根据controlnet模型返回预处理器列表
                 Object.defineProperty( this.widgets[styles_id], 'value', { get:()=>{ return controlnet },
                                                                            set:(value)=>{ controlnet = value; getprepro(selectorlist[1]) } })     
 
                 //根据选中状态返回预处理器
-                let style_select_values = ''  
                 Object.defineProperty(selector, "value", {
-                    set: (value) => {           
-                        selectorlist[1].querySelectorAll(".Preprocessor-tag").forEach(el => {
-                            if (value.split(',').includes(el.dataset.tag)) {
-                                el.classList.add("Preprocessor-tag-selected"); el.children[0].checked = true } }) },
+                    set: (value) => {
+                        setTimeout(_=>{ selectorlist[1].innerHTML = ''; selectorlist[1].append(...getTagList());
+                            selectorlist[1].querySelectorAll(".Preprocessor-tag").forEach(el => {
+                                if (value.split(',').includes(el.dataset.tag)) { el.classList.add("Preprocessor-tag-selected"); el.children[0].checked = true } }) 
+                            updatalist(selectorlist[1]); },300) },
                     get: () => {
                         selectorlist[1].querySelectorAll(".Preprocessor-tag").forEach(el => {
                             if(el.classList.value.indexOf("Preprocessor-tag-selected")>=0){
-                                if(!this.properties["values"].includes(el.dataset.tag)){ 
-                                    this.properties["values"].push(el.dataset.tag); }}
-                            else{ if(this.properties["values"].includes(el.dataset.tag)){ 
-                                     this.properties["values"]= this.properties["values"].filter(v=>v!=el.dataset.tag); } } });
-                        style_select_values = this.properties["values"].join(',');
-                        return style_select_values; } });
+                                if(!this.properties["values"].includes(el.dataset.tag)){ this.properties["values"].push(el.dataset.tag); }
+                            }else{ if(this.properties["values"].includes(el.dataset.tag)){
+                                    this.properties["values"]= this.properties["values"].filter(v=>v!=el.dataset.tag); } } });
+                        return this.properties["values"].join(','); }
+                });
 
                 getprepro(selectorlist[1]); return onNodeCreated;
             }
